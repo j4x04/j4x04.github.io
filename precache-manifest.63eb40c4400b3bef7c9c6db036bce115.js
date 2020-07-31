8814e6846e93be1dee1a704abd4d44af04be7230
Leo Zhang <leojxzhangg@gmail.com>
2020-07-31 19:54:53 -0400

    yes

---
+++ precache-manifest.63eb40c4400b3bef7c9c6db036bce115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878fb120723aabd36f1bfc73f27a4a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54f08065908854d544b4",
+    "url": "/static/css/main.9428272d.chunk.css"
+  },
+  {
+    "revision": "d71917487af96276c7ba",
+    "url": "/static/js/2.f0177961.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f0177961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f08065908854d544b4",
+    "url": "/static/js/main.3d85f913.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
